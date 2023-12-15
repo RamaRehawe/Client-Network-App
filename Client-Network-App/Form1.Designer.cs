22f8b4a240831118c5b9ee0f29a4fd9dbe6112a6
RamaRehawe <ramarehawe51@gmail.com>
2023-12-15 20:05:43 +0300

    Designing the client

--- Client-Network-App/Form1.Designer.cs
+++ Client-Network-App/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Client_Network_App
 {
-    partial class Form1
+    partial class _winFormClient
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,205 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this._statusTextBox = new System.Windows.Forms.TextBox();
+            this._connectButton = new System.Windows.Forms.Button();
+            this._disconnectButton = new System.Windows.Forms.Button();
+            this._portLabel = new System.Windows.Forms.Label();
+            this._ipAddressLabel = new System.Windows.Forms.Label();
+            this._portTextBox = new System.Windows.Forms.TextBox();
+            this._ipAddressTextBox = new System.Windows.Forms.TextBox();
+            this._sendCommandButton = new System.Windows.Forms.Button();
+            this._commandTextBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.75F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this._statusTextBox, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.22222F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.77778F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this._connectButton, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this._disconnectButton, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this._portLabel, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this._ipAddressLabel, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this._portTextBox, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this._ipAddressTextBox, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(477, 229);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.25926F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.74074F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(320, 218);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this._sendCommandButton, 1, 2);
+            this.tableLayoutPanel3.Controls.Add(this._commandTextBox, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(477, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(320, 220);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // _statusTextBox
+            // 
+            this._statusTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._statusTextBox.Location = new System.Drawing.Point(3, 3);
+            this._statusTextBox.Multiline = true;
+            this._statusTextBox.Name = "_statusTextBox";
+            this._statusTextBox.ReadOnly = true;
+            this.tableLayoutPanel1.SetRowSpan(this._statusTextBox, 2);
+            this._statusTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this._statusTextBox.Size = new System.Drawing.Size(468, 444);
+            this._statusTextBox.TabIndex = 2;
+            // 
+            // _connectButton
+            // 
+            this._connectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._connectButton.Location = new System.Drawing.Point(163, 170);
+            this._connectButton.Name = "_connectButton";
+            this._connectButton.Size = new System.Drawing.Size(154, 45);
+            this._connectButton.TabIndex = 0;
+            this._connectButton.Text = "Connect";
+            this._connectButton.UseVisualStyleBackColor = true;
+            // 
+            // _disconnectButton
+            // 
+            this._disconnectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._disconnectButton.Location = new System.Drawing.Point(3, 170);
+            this._disconnectButton.Name = "_disconnectButton";
+            this._disconnectButton.Size = new System.Drawing.Size(154, 45);
+            this._disconnectButton.TabIndex = 1;
+            this._disconnectButton.Text = "Disconnect";
+            this._disconnectButton.UseVisualStyleBackColor = true;
+            // 
+            // _portLabel
+            // 
+            this._portLabel.AutoSize = true;
+            this._portLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._portLabel.Location = new System.Drawing.Point(3, 135);
+            this._portLabel.Name = "_portLabel";
+            this._portLabel.Size = new System.Drawing.Size(154, 32);
+            this._portLabel.TabIndex = 2;
+            this._portLabel.Text = "Port: ";
+            this._portLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // _ipAddressLabel
+            // 
+            this._ipAddressLabel.AutoSize = true;
+            this._ipAddressLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._ipAddressLabel.Location = new System.Drawing.Point(3, 107);
+            this._ipAddressLabel.Name = "_ipAddressLabel";
+            this._ipAddressLabel.Size = new System.Drawing.Size(154, 28);
+            this._ipAddressLabel.TabIndex = 3;
+            this._ipAddressLabel.Text = "Ip Address";
+            this._ipAddressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // _portTextBox
+            // 
+            this._portTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._portTextBox.Location = new System.Drawing.Point(163, 138);
+            this._portTextBox.Name = "_portTextBox";
+            this._portTextBox.Size = new System.Drawing.Size(154, 24);
+            this._portTextBox.TabIndex = 4;
+            this._portTextBox.Text = "5000";
+            this._portTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // _ipAddressTextBox
+            // 
+            this._ipAddressTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._ipAddressTextBox.Location = new System.Drawing.Point(163, 110);
+            this._ipAddressTextBox.Name = "_ipAddressTextBox";
+            this._ipAddressTextBox.Size = new System.Drawing.Size(154, 24);
+            this._ipAddressTextBox.TabIndex = 5;
+            this._ipAddressTextBox.Text = "127.0.0.1";
+            this._ipAddressTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // _sendCommandButton
+            // 
+            this._sendCommandButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._sendCommandButton.Location = new System.Drawing.Point(163, 185);
+            this._sendCommandButton.Name = "_sendCommandButton";
+            this._sendCommandButton.Size = new System.Drawing.Size(154, 32);
+            this._sendCommandButton.TabIndex = 0;
+            this._sendCommandButton.Text = "Send Command";
+            this._sendCommandButton.UseVisualStyleBackColor = true;
+            // 
+            // _commandTextBox
+            // 
+            this.tableLayoutPanel3.SetColumnSpan(this._commandTextBox, 2);
+            this._commandTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._commandTextBox.Location = new System.Drawing.Point(3, 94);
+            this._commandTextBox.Multiline = true;
+            this._commandTextBox.Name = "_commandTextBox";
+            this._commandTextBox.Size = new System.Drawing.Size(314, 85);
+            this._commandTextBox.TabIndex = 1;
+            // 
+            // _winFormClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "_winFormClient";
+            this.Text = "WinForm Client";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox _statusTextBox;
+        private System.Windows.Forms.Button _connectButton;
+        private System.Windows.Forms.Button _disconnectButton;
+        private System.Windows.Forms.Label _portLabel;
+        private System.Windows.Forms.Label _ipAddressLabel;
+        private System.Windows.Forms.TextBox _portTextBox;
+        private System.Windows.Forms.TextBox _ipAddressTextBox;
+        private System.Windows.Forms.Button _sendCommandButton;
+        private System.Windows.Forms.TextBox _commandTextBox;
     }
 }
 
--- Client-Network-App/Form1.cs
+++ Client-Network-App/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Client_Network_App
 {
-    public partial class Form1 : Form
+    public partial class _winFormClient : Form
     {
-        public Form1()
+        public _winFormClient()
         {
             InitializeComponent();
         }
